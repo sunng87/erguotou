670cc7f30bc94933a5354de1a6d8b16c9a9531a8
Ning Sun <sunng@about.me>
2015-06-26 23:12:32 +0800

    (doc) update doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 #[deny(warnings)]
 
+//! # Erguotou
+//!
+//! Erguotou is a set of high-level utilities for working with http requests. Currently, Erguotou
+//! is fully based on [Hyper](https://hyperium.github.io/hyper/).
+//!
+
 extern crate hyper;
 extern crate rustc_serialize;
 extern crate url;

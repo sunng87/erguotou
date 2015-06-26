5d55a721243a6c12987b81247411d1a0b56c0bc4
Ning Sun <sunng@about.me>
2015-06-26 21:48:38 +0800

    (test) added form handler

--- examples/server.rs
+++ examples/server.rs
@@ -3,6 +3,7 @@ extern crate router;
 extern crate rustc_serialize;
 extern crate bodyparser;
 extern crate persistent;
+extern crate urlencoded;
 
 use std::str::FromStr;
 use iron::prelude::*;
@@ -10,6 +11,7 @@ use iron::status;
 use iron::headers::ContentType;
 use router::Router;
 use persistent::Read;
+use urlencoded::UrlEncodedBody;
 
 fn json_handler(req: &mut Request) -> IronResult<Response> {
     let body = req.get::<bodyparser::Raw>().ok().unwrap().unwrap();
@@ -19,9 +21,25 @@ fn json_handler(req: &mut Request) -> IronResult<Response> {
     Ok(resp.set(body).set(status::Ok))
 }
 
+fn form_handler(req: &mut Request) -> IronResult<Response> {
+    let mut resp = Response::new();
+    match req.get_ref::<UrlEncodedBody>() {
+        Ok(ref hashmap) => {
+            let body = format!("{:?}", hashmap);
+            resp.headers.set(ContentType(FromStr::from_str("text/plain").unwrap()));
+            Ok(resp.set(body).set(status::Ok))
+        },
+        Err(ref e) => {
+            println!("{:?}", e);
+            Ok(resp.set(status::BadRequest))
+        }
+    }
+}
+
 fn main() {
     let mut router = Router::new();
     router.post("/json", json_handler);
+    router.post("/form", form_handler);
 
     let mut chain = Chain::new(router);
     chain.link_before(Read::<bodyparser::MaxBodyLength>::one(10 * 1024 * 1024));
